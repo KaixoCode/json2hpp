36f0d60cde7823329be68a87649fa561752de76d
KaixoCode <jeroenzwiers2@gmail.com>
2022-09-30 21:06:13 +0200

    example

--- example/example.cpp
+++ example/example.cpp
@@ -1,9 +1,12 @@
 
 #include <array>
 #include <tuple>
+#include <string_view>
 
 #include "generated/data.hpp"
 
 int main() {
-    data.value;
+    constexpr auto item0 = data.menu.popup.menuitem[0];
+    constexpr auto id = data.menu.id;
+    constexpr auto onclick = data.menu.popup.menuitem[2].onclick;
 }
